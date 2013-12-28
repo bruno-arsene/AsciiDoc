d34dad499c3dc68d0396ede16cac882e232d2334
Nikoms <nikoms@gmail.com>
2013-12-29 00:58:54 +0100

    Ascii: Adding more tests & refactoring

--- Tests/Context/AsciiTest.php
+++ Tests/Context/AsciiTest.php
@@ -14,34 +14,35 @@ class AsciiTest extends \PHPUnit_Framework_TestCase
     public function testGetFirstElement()
     {
         $ascii = new Ascii('Hello world');
-        $this->assertSame('Hello world', $ascii->getCurrent());
+        $this->assertSame('Hello world', $ascii->fetch());
     }
 
 
     public function testFetchWithoutEol()
     {
         $ascii = new Ascii("Hello world");
+        $ascii->fetch();
         $this->assertFalse($ascii->fetch());
     }
 
     public function testFetchOnce()
     {
         $ascii = new Ascii("Hello world\nOk");
-        $this->assertTrue($ascii->fetch());
+        $this->assertSame('Hello world', $ascii->fetch());
     }
 
     public function testSplitWithBackSlashN()
     {
         $ascii = new Ascii("Hello world\nYes!");
         $ascii->fetch();
-        $this->assertSame('Yes!', $ascii->getCurrent());
+        $this->assertSame('Yes!', $ascii->fetch());
     }
 
     public function testSplitWithPHPEOL()
     {
         $ascii = new Ascii("Hello world" . PHP_EOL . "Yes!");
         $ascii->fetch();
-        $this->assertSame('Yes!', $ascii->getCurrent());
+        $this->assertSame('Yes!', $ascii->fetch());
     }
 
     public function testSplitWithReturn()
@@ -51,14 +52,38 @@ class AsciiTest extends \PHPUnit_Framework_TestCase
 Yes!"
         );
         $ascii->fetch();
-        $this->assertSame('Yes!', $ascii->getCurrent());
+        $this->assertSame('Yes!', $ascii->fetch());
     }
 
-    public function testFetchDouble()
+    public function testFetchToLast()
     {
         $ascii = new Ascii("Hello world\nYes!\nOk");
         $ascii->fetch();
         $ascii->fetch();
-        $this->assertSame('Ok', $ascii->getCurrent());
+        $this->assertSame('Ok', $ascii->fetch());
+    }
+
+    public function testLinesCount(){
+        $ascii = new Ascii("1\n2\n3\n4\n5");
+        $i = 0;
+        while($ascii->fetch() !== false){
+            $i++;
+        }
+        $this->assertSame(5,$i);
+    }
+
+    public function testEmptyString(){
+        $ascii = new Ascii("");
+        $this->assertSame('',$ascii->fetch());
+        $this->assertSame(false,$ascii->fetch());
+    }
+
+    public function testEmptyLines(){
+        $ascii = new Ascii("\n\n\n\n");
+        $i = 0;
+        while($ascii->fetch() !== false){
+            $i++;
+        }
+        $this->assertSame(5,$i);
     }
 }
--- src/AsciiDoc/Context/Ascii.php
+++ src/AsciiDoc/Context/Ascii.php
@@ -9,24 +9,31 @@ namespace AsciiDoc\Context;
 class Ascii implements Contextable
 {
 
-    private $input;
+    private $lines;
     private $currentLine;
 
     public function __construct($text)
     {
         $normalizedEolText = str_replace("\r\n", "\n", $text);
-        $this->input = explode("\n", $normalizedEolText);
-        $this->currentLine = 0;
+        $this->lines = explode("\n", $normalizedEolText);
+        $this->currentLine = -1;
     }
 
     public function getCurrent()
     {
-        return $this->input[$this->currentLine];
+        return $this->getLine($this->currentLine);
     }
 
     public function fetch()
     {
-        $this->currentLine++;
-        return $this->currentLine < count($this->input);
+        ++$this->currentLine;
+        return $this->getCurrent();
+    }
+
+    private function getLine($line){
+        if($line >= 0 && $line < count($this->lines)){
+            return $this->lines[$line];
+        }
+        return false;
     }
 }
